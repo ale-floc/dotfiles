8c0ffb4b9112c0e8feaa781b2d70d41dde8fc589
alf <kaboffbeta@gmail.com>
2021-09-30 17:58:04 +0000

    Add re-install of ansible

--- install.sh
+++ install.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/env bash
 
 function get_dotfiles () {
+    rm -rf /tmp/dotfiles
     git clone https://github.com/ale-floc/dotfiles.git /tmp/dotfiles
     cd /tmp/dotfiles && ansible-playbook playbook.yml --ask-become-pass
 }
 
-prompt=$(sudo -nv 2>&1)
-if echo $prompt | grep -q '^sudo:'; then
-  sudo -y apt install curl git ansible
-else
-  apt -y install curl git ansible
-fi
-
+sudo apt -y install curl git ansible
 get_dotfiles
